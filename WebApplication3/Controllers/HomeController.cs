d186a202f46b359396b97bb774fa3e26e0479d2e
Batuhan FINDIK <batuhanf@med-case.io>
2023-01-01 17:04:10 +0300

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult GetHomeIndexTable()
+        {
+            var model = new List<HomeIndexDTO>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                HomeIndexDTO homeIndexDTO = new HomeIndexDTO()
+                {
+                    Id = i,
+                    Name = "Test" + i
+                };
+                model.Add(homeIndexDTO);
+            }
+
+            return PartialView("~/Views/partialViews/_homeindexpartialview.cshtml", model);
+        }
+        public JsonResult AddHesapKartTip([FromForm] HomeIndexDTO homeIndexDTO)
+        {
+            Object result;
+            if (homeIndexDTO.Name == null || homeIndexDTO.Name == "")
+                result = new { result = false };
+            else
+                result = new { result = true };
+
+           return Json(result);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication3/wwwroot/js/site.js
@@ -0,0 +1,41 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+var hesapkarttip = {
+    listele: function() {
+        $.ajax({
+            url: "https://localhost:7120/Home/GetHomeIndexTable",
+            type: 'GET',
+            success: function (res) {
+                $('#homeindexmain').html("");
+                $('#homeindexmain').html(res);
+                console.log('dadasda')
+            }
+        });
+    },
+    Ekle:function(){
+        $('#hesapTipEkle').click(function () {
+            var data = $("#myModal .modal-body form").serialize();
+
+            $.ajax({
+                url: "https://localhost:7120/Home/AddHesapKartTip",
+                data: data,
+                type: "POST",
+                success: function (res) {
+                    if (res.result) {
+                        swal("Kayıt Başarılı!", "", "success");
+                        $("input[name='Name']").val("");
+                        hesapkarttip.listele();
+                    } else {
+                        swal("Kayıt Başarısız!", "", "error");
+                    }
+                }, error: function (hata, ajaxoptions, throwerror) {
+                    alert("Hata :" + hata.status + " " + throwerror + " " + hata.responseText);
+                }
+            });
+        });
+    }
+}
